d7e591ecf74b86f6b1804734c4c1ad26549c849b
unknown <Jukev>
2025-05-08 08:38:56 +0100

    Add tableName to user and transaction model

--- models/transaction.js
+++ models/transaction.js
@@ -58,6 +58,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: 'Transaction',
+      tableName: 'Transactions',
       underscored: true,
     }
   );
--- models/user.js
+++ models/user.js
@@ -105,6 +105,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    tableName: 'Users',
   });
   return User;
 };
