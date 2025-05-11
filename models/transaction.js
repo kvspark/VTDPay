0d75f3b6e245d84fcd1b45d077ee003718081ab0
unknown <Jukev>
2025-05-11 21:12:05 +0100

    change table name to smail t for tranasctions

--- models/transaction.js
+++ models/transaction.js
@@ -58,7 +58,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: 'Transaction',
-      tableName: 'Transactions',
+      tableName: 'transactions',
       underscored: true,
     }
   );
