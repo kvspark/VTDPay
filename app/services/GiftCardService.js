7ba413de26a480bdf8e1f92a655bf6e9686189e2
unknown <Jukev>
2025-05-21 19:10:22 +0100

    Associate add to gift card service for getbydi

--- app/services/GiftCardService.js
+++ app/services/GiftCardService.js
@@ -12,7 +12,12 @@ class GiftCardService {
   };
 
   static getGiftCardById = async (id) => {
-    const giftCard = await GiftCard.findByPk(id);
+    const giftCard = await GiftCard.findByPk(id, {
+      include: {
+        model: GiftCardCurrency,
+        as: 'currencies'
+      }
+    });
     if (!giftCard) throw new Error('Gift card not found');
     return giftCard;
   };
